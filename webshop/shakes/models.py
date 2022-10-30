fc0b20de8d5e0ce4dec92ccaf019330d1c48a024
Quinten <90308786+QNoah@users.noreply.github.com>
2022-10-30 22:48:52 +0100

    Update: Loading data
    only need to style the data, importing data is finished for shakes.

--- webshop/shakes/models.py
+++ webshop/shakes/models.py
@@ -1,10 +1,11 @@
+from this import d
 from django.db import models
 
 class shakes(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=5, decimal_places=2)
     description = models.TextField()
-    image = models.ImageField(upload_to='images/')
+    image = models.ImageField(upload_to='images/', default='images/default.jpg')
 
     def __str__(self):
         return self.name
